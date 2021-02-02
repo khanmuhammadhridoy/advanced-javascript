d507e778cf5b34c9abcee0041382e09a4da5bcbe
khanmuhammadhridoy <khanmuhammadhridoy@gmail.com>
2021-02-02 09:39:39 +0600

    double vs triple equal

---
+++ double vs triple equal.js
@@ -0,0 +1,8 @@
+double equal (==) just compare value
+where 0=false, eventhough 0 is Number, false is Boolean
+same in 1 = true
+also number and number in string are same
+like 0 = '0'
+
+triple equal (===) it will compare not only value but also type
+so there 0 and false will not be equal cause type doesn't match
