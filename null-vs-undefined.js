5265d3e675263c9f73151d6943a5446aa94d7e08
khanmuhammadhridoy <khanmuhammadhridoy@gmail.com>
2021-02-02 09:22:26 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,39 @@
+// for null we can set value to null
+
+
+// undefined
+// let name; /*didn't set value*/
+/*function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}*/
+/*
+    function return kori nai
+    or return e value dei nai
+    in both case it will give us undefined and also gives result
+ */
+/*function add(num1, num2){
+    console.log(num2);
+}*/
+/*
+    function e num2 value set na korle undefined asbe
+    like 
+    const result = add (2); //2 is num1 value
+    console.log(result);
+*/
+/*
+const name = {name:"name", no: 353535};
+console.log(name.home); //object e undefined
+*/
+
+let neverDoIt =undefined; // set to undefined :)
+
+let ages= [2,6,5];
+console.log(ages[21]);// array te asei 3ta 21 no koi pamu :)
+
+// const result = add(1, 2);
+// console.log(result);
+
+
+
+
