8e6a8bad450ce339a314082f8a8892076e8ff01c
khanmuhammadhridoy <khanmuhammadhridoy@gmail.com>
2021-02-05 16:22:42 +0600

    double-vs-triple-equal

--- double-vs-triple-equal.js
+++ double-vs-triple-equal.js
@@ -1,3 +1,5 @@
+
+/*
 double equal (==) just compare value
 where 0=false, eventhough 0 is Number, false is Boolean
 same in 1 = true
@@ -6,4 +8,22 @@ like 0 = '0'
 
 triple equal (===) it will compare not only value but also type
 so there 0 and false will not be equal cause type doesn't match
+*/
+const num1 = 1;
+const num2 = true;
+// double equal
+if (num1 == num2) {
+    console.log('Double equal condition is true');
+}
+else{
+    console.log('Double equal condition is false');
 
+}
+// triple equal
+if (num1 === num2) {
+    console.log('triple equal condition is true');
+}
+else{
+    console.log('triple equal condition is false');
+
+}
